c626ee92f0dc371b66092fcfb83adbd14228526c
Milly <wmr4job@gmail.com>
2023-12-07 18:56:39 +0800

    Update commit

--- random_guess_number.py
+++ random_guess_number.py
@@ -1,16 +1,21 @@
 #random_guess number 
 import random
-r = random.randint(1,100) #隨機產生一個1~100間的整數，範圍包含1、100
-guess = int(input('終極密碼1~100，猜猜看：'))
-now_down = 0
-now_up = 100
-while guess != r:
-	if guess > r:
+
+now_down = int(input('請輸入數字範圍最小值'))
+now_up = int(input('請輸入數字範圍最大值'))
+r = random.randint(now_down,now_up) #隨機產生一個1~100間的整數，範圍包含1、100
+n = 0 #猜了幾次
+
+while True:
+	guess = int(input('終極密碼1~100，猜猜看：'))
+	n += 1
+	if guess == r:
+		print('砰！爆炸啦！', '這是你猜的第', n, '次')
+		break
+	elif guess > r:
 		print(now_down, '~', guess)
 		now_up = guess
-		guess = int(input('終極密碼，猜猜看：'))
 	else:
 		print(guess, '~', now_up)
 		now_down = guess
-		guess = int(input('終極密碼，猜猜看：'))
-print('砰！爆炸啦！')
+	print('這是你猜的第', n, '次')
